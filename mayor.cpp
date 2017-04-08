7953621a3e68853ebd05e599770388127392c9b5
sebaceaort <sceaort@gmail.com>
2017-04-08 17:03:46 -0300

    Add files via upload

---
+++ mayor.cpp
@@ -0,0 +1,29 @@
+// Nombre     : Mayor.cpp
+// Descripcin: Dados dos numeros indicar cual es el mayor
+// Alumnos    : Sebastian Cea, Leg: 1580048
+// Fecha      : 20170407
+
+
+#include<iostream>
+int main()
+{
+    int numero1;
+    int numero2;
+
+    std::cout<<"Escriba dos enteros para comparar\n";
+    std::cin>>numero1>>numero2;
+
+        if(numero1==numero2){
+            std::cout<<numero1<<" es igual a "<<numero2;
+        }
+            else{
+
+                    if(numero1>numero2){
+                        std::cout<<numero1<<" es mayor a "<<numero2;
+                        }
+                            else{
+                                    std::cout<<numero2<<" es mayor a "<<numero1;
+                                }
+                }
+}
+
